5666f2674387348ef2365e95b6e7db4680fd3f29
gijs.nijholt <gijs.nijholt@476a50c0-da5d-5340-8c75-4ad663a144f1>
2011-11-11 17:08:39 +0000

    Clean up comments

--- lizard_sticky_twitterized/views.py
+++ lizard_sticky_twitterized/views.py
@@ -19,35 +19,3 @@ class StickyBrowserView(AppView):
         return StickyTweet.objects.filter(visible=True).order_by('-created_on')
         
     template_name = 'lizard_sticky_twitterized/sticky_twitterized-browser.html'
-
-
-
-
-
-# def sticky_browser(
-#     request,
-#     template='lizard_sticky_twitterized/sticky_twitterized-browser.html',
-#     crumbs_prepend=None):
-#     """Show sticky browser.
-# 
-#     Automatically makes new workspace if not yet available
-# 
-#     """
-#     if crumbs_prepend is not None:
-#         crumbs = list(crumbs_prepend)
-#     else:
-#         crumbs = [{'name': 'home', 'url': '/'}]
-#     crumbs.append(
-#         {'name': 'meldingen',
-#          'url': reverse('lizard_sticky_twitterized.sticky_browser')})
-# 
-#     tweets = StickyTweet.objects.filter(visible=True).order_by('-created_on')[:5]
-# 
-#     return render_to_response(
-#         template,
-#         {'javascript_hover_handler': 'popup_hover_handler',
-#          'javascript_click_handler': 'sticky_popup_click_handler',
-#          'tweets': tweets,
-#          'crumbs': crumbs,
-#          },
-#         context_instance=RequestContext(request))
