dcce3e6ea4849afb815573b002ae1961d0b919a2
Remco Gerlich <remco@gerlich.nl>
2014-09-08 14:28:05 +0200

    Staticfiles

--- lizard_sticky_twitterized/urls.py
+++ lizard_sticky_twitterized/urls.py
@@ -20,7 +20,7 @@ if getattr(settings, 'LIZARD_TWITTER_STANDALONE', False):
         '',
         (r'^map/', include('lizard_map.urls')),
         (r'^ui/', include('lizard_ui.urls')),
-        (r'', include('staticfiles.urls')),
+        (r'', include('django.contrib.staticfiles.urls')),
         (r'^admin/', include(admin.site.urls)),
     )
 
